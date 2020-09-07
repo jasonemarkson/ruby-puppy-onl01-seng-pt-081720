8b1d2d86d8a86f6b95b20afd5d84e9d1df25c8af
Jason Markson <jasonmarkson76@gmail.com>
2020-09-07 16:34:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # Add your code here
+require 'pry'
+
+class Dog 
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.clear_all
+    @@all.clear 
+  end
+  
+  binding.pry 
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts dog
+    end
+  end
+end
